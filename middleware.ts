27938d9f53d889213b036e81da772a9cb30c2111
clquek67 <clquek@gmail.com>
2026-07-06 11:12:08 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      db: { schema: 'one_empire_pm' },
       cookies: {
         getAll() { return request.cookies.getAll() },
         setAll(cookiesToSet) {
